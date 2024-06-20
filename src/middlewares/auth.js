873c4ec9106cc38ff6163124b8f52648f0cd129b
TrapitoKid <vichidiaz424@gmail.com>
2024-06-20 14:53:11 -0400

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,3 +5,14 @@ export const isAuthenticated = (req, res, next) => {
     res.redirect('/login')
 }
 
+export const isAdmin = (req, res, next) => {
+    /* Vamos a utilizar este middleware luego de que se ejecute 
+    el de authenticate, por lo quepodemos acceder a req.user
+    (ya que authenticate) llena con la info del usuario */
+    const role = req.user.token.role
+    if(role === 'ADMIN'){
+        next()
+    } else {
+        return res.send('No autorizado!')
+    }
+}
